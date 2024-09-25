e778a42d46ae0118b0acc2ebdae8cf0706f25d11
pradeep149 <npradeep9573@gmail.com>
2024-09-25 20:56:22 +0530

    frontend commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  external: ["@mui/material"],
   plugins: [react()],
-  server: {port:5173}
-})
+  server: { port: 5173 },
+});
