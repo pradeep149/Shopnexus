8826ca9295166a35da73c056f414ac7c55c39205
pradeep149 <npradeep9573@gmail.com>
2024-09-26 11:13:33 +0530

    edited server.js file

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ import passport from "passport";
 import session from "express-session";
 import "./config/passport.js";
 import jwt from "jsonwebtoken";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -36,6 +38,8 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/wishlist", wishlistRouter);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
